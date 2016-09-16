dd3f1fd59f55ecbe850a910a89f29c185d6eec20
luhtonen <luhtonen@gmail.com>
2016-09-16 14:00:57 +0200

    Simple "Hello, Web" application

--- main.go
+++ main.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
+	"time"
 )
+
+const (
+	Port = ":8080"
+)
+
+func serveDynamic(w http.ResponseWriter, r *http.Request) {
+	response := "The time is now " + time.Now().String()
+	fmt.Fprintln(w, response)
+}
+
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static.html")
+}
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 }
@@ -12,6 +26,9 @@ func TestHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/test", TestHandler)
+	router.HandleFunc("/", serveDynamic)
+	router.HandleFunc("/static", serveStatic)
 	http.Handle("/", router)
-	fmt.Println("Everything is set up!")
+	fmt.Println("Listening on port", Port)
+	http.ListenAndServe(Port, nil)
 }
