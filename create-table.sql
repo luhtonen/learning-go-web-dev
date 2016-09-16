a8006454428dfe4818ee73ec9daf246e7848bd83
luhtonen <luhtonen@gmail.com>
2016-09-16 19:25:44 +0200

    Connecting to database

---
+++ create-table.sql
@@ -0,0 +1,19 @@
+-- connect to database: mysql -u root -p
+-- create database
+-- CREATE DATABASE cms;
+-- create database user
+-- GRANT ALL ON cms.* TO cms@localhost IDENTIFIED BY 'cms123';
+
+-- create table
+CREATE TABLE `pages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `page_guid` varchar(256) NOT NULL DEFAULT '',
+  `page_title` varchar(256) DEFAULT NULL,
+  `page_content` mediumtext,
+  `page_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `page_guid` (`page_guid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- insert 1 row
+INSERT INTO `pages` (`id`, `page_guid`, `page_title`, `page_content`, `page_date`) VALUES (NULL, 'hello-world', 'Hello, World', 'I\'m so glad you found this page!  It\'s been sitting patiently on the Internet for some time, just waiting for a visitor.', CURRENT_TIMESTAMP);
--- main.go
+++ main.go
@@ -1,31 +1,56 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
-	"os"
 )
 
 const (
-	Port = ":8080"
+	Port    = ":8080"
+	DBHost  = "127.0.0.1"
+	DBPort  = ":3306"
+	DBUser  = "cms"
+	DBPass  = "cms123"
+	DBDbase = "cms"
 )
 
-func pageHandler(w http.ResponseWriter, r *http.Request) {
+var database *sql.DB
+
+type Page struct {
+	Title   string
+	Content string
+	Date    string
+}
+
+func servePage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	pageID := vars["id"]
-	fileName := "files/" + pageID + ".html"
-	_, err := os.Stat(fileName)
+	thisPage := Page{}
+	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE id=?", pageID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
 	if err != nil {
-		fmt.Println("error caught:", err.Error())
-		fileName = "files/404.html"
+		fmt.Println("Couldn't get page:", pageID, err.Error())
 	}
-	http.ServeFile(w, r, fileName)
+	html := `<html><head><title>` + thisPage.Title + `</title></head><body><h1>` + thisPage.Title + `</h1><div>` + thisPage.Content + `</div></body></html>`
+	fmt.Fprintln(w, html)
+}
+
+func initDB() {
+	dbConn := fmt.Sprintf("%s:%s@tcp(%s%s)/%s", DBUser, DBPass, DBHost, DBPort, DBDbase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("Could not connect to database", err.Error())
+	}
+	database = db
 }
 
 func main() {
+	initDB()
 	router := mux.NewRouter()
-	router.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	router.HandleFunc("/pages/{id:[0-9]+}", servePage)
 	http.Handle("/", router)
 	fmt.Println("Listening on port", Port)
 	http.ListenAndServe(Port, nil)
